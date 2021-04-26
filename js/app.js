d9a8a832e19e46edb7495512d8dd270e939164b5
AhmadNAwasrah <eng.ahmad.nawasreh@gmail.com>
2021-04-26 17:00:03 +0300

    done

--- js/app.js
+++ js/app.js
@@ -3,90 +3,130 @@
 "use strict";
 
 
-
 alert('Welcome to my personal web site ');
 
-let age =28;
-var gussedAge =prompt('Guess about my age numbers only ');
+let age = 28;
+var gussedAge = prompt('Guess about my age numbers only ');
 
-if(age==gussedAge)
-{alert(true);
-console.log(true);
+if (age == gussedAge) {
+    alert(true);
+    console.log(true);
 }
-else
-{
+else {
     alert('You enter wrong answer')
     console.log(false);
 
 }
 
-var garduatedUnivercity=prompt('Guess from wich univercity I was grdduated just yarmouk ju ').toLocaleLowerCase;
-switch(garduatedUnivercity)
-{
-    case just:
-    alert('True')
+// var garduatedUnivercity = prompt('Guess from wich univercity I was grdduated just yarmouk ju ').toLowerCase;
+// if (garduatedUnivercity) {
+
+//     if (garduatedUnivercity == "just") {
+//         alert('True')
+//         console.log(true);
+
+//     } else if (garduatedUnivercity = "yarmouk") {
+//         alert('fales')
+//         console.log(fales);
+//     }
+
+
+// }
+// else {
+
+//     alert('You enter wrong chooice  ')
+// }
+
+
+
+
+// else {
+//     alert('You enter wrong chooice  ')
+//     console.log('wrong choise');
+// }
+
+
+
+
+
+
+
+// switch(garduatedUnivercity)
+
+
+
+
+// {
+//     case  'just' : 
+//         //alert('True')
+//         console.log(true);
+//         break;
+
+//     case 'yarmouk':
+
+//         //('fales ')
+//         console.log(false);
+//         break;
+
+//     case 'ju' :
+
+//         //('fales ')
+//         console.log(false);
+//         break;
+
+//     default : 
+//         //alert('You enter wrong chooice  ')
+//         console.log('wrong choise');
+// }
+
+
+
+var smokingMan = prompt('Do you think I\m smoking man ? tab y for yes and n for no ').toLowerCase;
+if (smokingMan == 'n') {
+    alert('True');
     console.log(true);
-    break;
-
-    case yarmouk :
-        
-        alert('fales ')
-        console.log(false);
-        break;
-        case ju :
-        
-        alert('fales ')
-        console.log(false);
-        break;
-
-        default : 
-        alert('You enter wrong chooice  ')
-        console.log('wrong choise');
 }
-
-
-
-var smokingMan = prompt('Do you think I\m smoking man ? tab y for yes and n for no ').toLocaleLowerCase;
-if(smokingMan=='n') 
-{alert('True');
-console.log(true);}
 else {
     alert('Fales');
-console.log(false);
+    console.log(false);
 }
 
-var marridMan = prompt('Do you think I\m married man ? tab y for yes and n for no ').toLocaleLowerCase;
-if(marridMan=='n') 
-{
+var marridMan = prompt('Do you think I\m married man ? tab y for yes and n for no ').toLowerCase;
+if (marridMan == 'n') {
     alert('True');
-console.log(true);
+    console.log(true);
 }
-else 
-{
+else {
     alert('Fales');
-console.log(false);
+    console.log(false);
 }
 
-var employerMan = prompt('Do you think I\m employer man ? tab y for yes and n for no ').toLocaleLowerCase;
-if(employerMan=='n') 
-{
+var employerMan = prompt('Do you think I\m employer man ? tab y for yes and n for no ').toLowerCase;
+if (employerMan == 'n') {
     alert('True');
-console.log(true);
+    console.log(true);
 }
-else 
-{
+else {
     alert('Fales');
-console.log(false);
+    console.log(false);
 }
 
-var tallMan = prompt('Do you think I\m tall man ? tab y for yes and n for no ').toLocaleLowerCase;
-if(tallMan=='y') 
-{
+var tallMan = prompt('Do you think I\m tall man ? tab y for yes and n for no ').toLowerCase;
+if (tallMan == 'y') {
     alert('True');
-console.log(true);
+    console.log(true);
 }
-else 
-{
+else {
     alert('Fales');
-console.log(false);
+    console.log(false);
+}
+
+var fateMan = prompt('Do you think I\m fate man ? tab y for yes and n for no ').toLowerCase;
+if (fateMan == 'y') {
+    alert('True');
+    console.log(true);
+}
+else {
+    alert('Fales');
+    console.log(false);
 }
