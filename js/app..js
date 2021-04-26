6fa23a119ffef0329b95f993b92e9853f5d7bcf0
AhmadNAwasrah <eng.ahmad.nawasreh@gmail.com>
2021-04-26 14:21:46 +0300

    add all projects file with css and js

---
+++ js/app..js
@@ -0,0 +1,21 @@
+
+
+"use strict";
+
+
+
+alert('Welcome to my personal web site ');
+
+let age =28;
+var gussedage =prompt('Guess about my age numbers only ');
+
+if(age==gussedage)
+{alert(true);
+console.log(true);
+}
+else
+{
+    alert('You enter wrong answer')
+    console.log(false);
+
+}
