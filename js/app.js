ce1012cc8c269ce98780c4ddf9bc4c82a35ca976
AhmadNAwasrah <eng.ahmad.nawasreh@gmail.com>
2021-04-27 18:22:45 +0300

    lab2 done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
 
 "use strict";
+let scoure=0;
 
 
 alert('Welcome to my personal web site ');
@@ -11,6 +12,7 @@ var gussedAge = prompt('Guess about my age numbers only ');
 if (age == gussedAge) {
     alert(true);
     console.log(true);
+    scoure++;
 }
 else {
     alert('You enter wrong answer')
@@ -85,6 +87,8 @@ var smokingMan = prompt('Do you think I\m smoking man ? tab y for yes and n for 
 if (smokingMan == 'n') {
     alert('True');
     console.log(true);
+    scoure++;
+
 }
 else {
     alert('Fales');
@@ -95,6 +99,8 @@ var marridMan = prompt('Do you think I\m married man ? tab y for yes and n for n
 if (marridMan == 'n') {
     alert('True');
     console.log(true);
+    scoure++;
+
 }
 else {
     alert('Fales');
@@ -105,6 +111,8 @@ var employerMan = prompt('Do you think I\m employer man ? tab y for yes and n fo
 if (employerMan == 'n') {
     alert('True');
     console.log(true);
+    scoure++;
+
 }
 else {
     alert('Fales');
@@ -115,6 +123,8 @@ var tallMan = prompt('Do you think I\m tall man ? tab y for yes and n for no ').
 if (tallMan == 'y') {
     alert('True');
     console.log(true);
+    scoure++;
+
 }
 else {
     alert('Fales');
@@ -125,8 +135,76 @@ var fateMan = prompt('Do you think I\m fate man ? tab y for yes and n for no ').
 if (fateMan == 'y') {
     alert('True');
     console.log(true);
+    scoure++;
+
 }
 else {
     alert('Fales');
     console.log(false);
 }
+
+let favNumber=prompt('Guess my favorite number between 1-5?');
+let count=1;
+do 
+{
+if(favNumber===3)
+{alert('Exsactly ');
+console.log('Exsactly');
+scoure++;
+
+}
+else if(favNumber===2)
+{alert('too high')}
+eles
+ if(favNumber===1)
+{alert('too low');
+console.log('too low');
+}
+else if(favNumber===4)
+{alert('too high')
+
+console.log('too high')}
+
+
+else
+{
+alert('too low');
+console.log('too low');
+
+}
+count++;
+}
+while(count<4);
+
+alert('My favorate number is 3 ');
+
+let favpalce=['amman','irbid','ajloun','jersh','aqaba'];
+
+let guessFavPlace=prompt('Pleas guess my favorate ');
+let flage=fales;
+for(i=0;i<favpalce.length.i++)
+{
+    if(guessFavPlace==favpalce[i]){
+alert('Exsactly ');
+console.log('exsactly');
+flage =true;
+scoure++;
+
+break ;
+
+}
+
+}
+
+if(flage==false)
+{alert('sorry my favorate places is amman irbid ajloun jersh aqaba');
+console.log('sorry my favorate places is amman irbid ajloun jersh aqaba');}
+
+
+alert('your final Score is '+ scoure);
+
+
+
+
+
+
